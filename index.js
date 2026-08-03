136327f6a66aca5e7173f3faf3c1d087eae3ec63
khalidabdifetah9 <khalidabdifetah9@gmail.com>
2026-08-03 10:21:27 +0300

    version 2

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-let nu = document.querySelectorAll(".inp");
-const form = document.querySelector(".formm");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const data = new FormData(form);
-    console.log(data);
-    
-    // for (const value of data.values()) {
-    //     console.log(value);
-    // }
-});
+console.log("version 2");
+console.log("version 2 again")
